031d741ddaeed1de0ae1b9a413f4c3c3bc1eb50a
ADSO <nibyminson@gmail.com>
2025-02-24 11:22:15 -0500

    Version 0.1 creo el modulo de la tabla usuarios

---
+++ back/models/usuario.js
@@ -0,0 +1,33 @@
+/**
+@file usuario.js
+@description Modúlo de la base de datos para la tabla usuario.
+@param Schema_Model instanciar la libreria en una constante
+@param Usuario_schema instanciar constante con los datos que tendrá la base de datos
+@param Collection crea la colección en mongoDB con el nombre indicado "usuario"
+@param Exports exporta el modelo con el nombre indicado y la función deseada
+*/
+const{Schema, model} = require('mongoose')
+const usuario_schema = Schema(
+    {
+        nombre_usuario: {
+          type: String,
+          required: true,
+        },
+        correo_usuario: {
+          type: String,
+          required: true,
+        },
+        password_usuario: {
+          type: String,
+          required: true,
+        },
+        is_admin: {
+          type: Boolean,
+          default: false,
+        },
+        pais_usuario: {
+          type: String,
+          default: "",
+        }
+    },{ collection: "usuario" });
+    module.exports=model("usuario",usuario_schema);
