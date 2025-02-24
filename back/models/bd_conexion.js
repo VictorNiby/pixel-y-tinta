2e27a0b74b5d7e7071a7984b4267685557a3c031
ADSO <nibyminson@gmail.com>
2025-02-24 10:57:40 -0500

    Inicio del blog, bases MVC y librerias NODE JS

--- back/models/bd_conexion.js
+++ back/models/bd_conexion.js
@@ -1,4 +1,16 @@
 //Instanciar  la libreria mongoose
+/**
+
+@file conexion.js
+
+@description
+
+Módulo para establecer la conexión a la base de datos MongoDB usando Mongoose.
+
+Proporciona una función asincrónica que gestiona posibles errores durante la conexión.
+*/
+
+const mongoose = require('mongoose');
 const mongoose = require('mongoose');
 const conexion = async (e)=>{
     try {
---
+++ front/index.py
@@ -0,0 +1,1 @@
+def testeo = 0
