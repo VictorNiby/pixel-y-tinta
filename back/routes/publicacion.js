267d1eb8542778c3cb55ce8ee28b03858bffc1ca
ADSO <nibyminson@gmail.com>
2025-02-26 08:07:32 -0500

    Version 0.3.1 correcion en las rutas de la publicacion

--- back/routes/publicacion.js
+++ back/routes/publicacion.js
@@ -10,15 +10,15 @@ router.post(
   control_publicacion.crear_publicacion
 );
 router.get(
-  "/publicacion/buscar_publicacion_id/id:",
+  "/publicacion/buscar_publicacion/id:",
   control_publicacion.buscar_publicacion_id
 );
 router.delete(
-  "/publicacion/borrar_publicacion_id/:id",
+  "/publicacion/borrar_publicacion/:id",
   control_publicacion.borrar_publicacion_id
 );
 router.put(
-  "/publicacion/actualizar_publicacion_id/:id",
+  "/publicacion/actualizar_publicacion/:id",
   control_publicacion.actualizar_publicacion_id
 );
 module.exports = router;
