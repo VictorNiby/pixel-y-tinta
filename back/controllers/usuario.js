0cb49ebbfe74db7ccc2a530e6ef54d917b905a86
DiegoValdesR <bitardos8@gmail.com>
2025-02-27 17:41:33 -0500

    Version 0.9 Funcion Log In y verificar correo de usuarios listo, ademas de la documentacion

--- back/controllers/usuario.js
+++ back/controllers/usuario.js
@@ -1,4 +1,26 @@
-let usuarios = require('../models/usuario.js')
+/**
+ * @function usuario_controller
+ * @var usuario - Importamos el modelo.
+ * @var  bcrypt - Instanciamos la libreria bcrypt para encriptar contraseñas.
+ * @constant jwt - Instanciamos la libreria jsonwebtoken para generar un token para el login.
+ * @param {req,resp} - Request de la api y Response que se envia al servidor.
+ * @constant listar_todos - Enlista en un array de objetos toda la información de los usuarios en la base de datos.
+ * @constant listar_por_id - Enlista en un objeto la informacion de un solo usuario con un id especifico.
+ * @constant insertar_usuario - Del request llega un objeto con toda la información a insertar, busca si el correo ingresado ya existe
+ * y si no existe inserta un nuevo usuario.
+ * @constant editar_usuario - Edita un usuario especifico (con el id el cual es enviado desde el request por medio de la url) 
+ * con datos enviados del request.
+ * @constant eliminar_usuario - Recibe un id y lo utiliza para borrar un usuario específico.
+ * @constant recuperar_password - Por medio de la url el sistema recibirá un correo, y por el body se enviará una contraseña
+ * para reemplazar la anterior, el sistema comprobará si el correo enviado si existe en la base de datos y obtendrá el id del
+ * registro con ese correo, y con ese id actualizará la contraseña del usuario a la nueva.
+ * @constant log_in - Por el body del request se envian la contraseña y el correo, el sistema va a usar el correo para identificar 
+ * que usuario esta intentando iniciar sesion en el sistema, si el correo enviado coincide con uno registrado, el sistema comparará
+ * la contraseña enviada con la encriptada en la base de datos, y si coinciden el usuario podrá acceder a su cuenta en la página.
+ * @constant verificar_correo - Recibe un correo por la url y retorna true si existe en la bd, false de lo contrario
+ */
+
+let usuario = require('../models/usuario.js')
 let bcrypt = require('bcryptjs')
 
 const jwt = require('jsonwebtoken')
@@ -6,7 +28,7 @@ const jwt = require('jsonwebtoken')
 
 const listar_todos = async(req,resp)=>{
     try {
-        let listar_usuarios = await usuarios.find().exec()
+        let listar_usuarios = await usuario.find().exec()
         resp.status(200).send({
             completado:true,
             mensaje:"Consulta ejecutada exitosamente",
@@ -25,7 +47,7 @@ const listar_por_id  = async (req,resp) => {
     let id = req.params.id
 
     try {
-        let consulta = await usuarios.findById(id).exec()
+        let consulta = await usuario.findById(id).exec()
 
         return resp.send({
             completado: true,
@@ -54,7 +76,7 @@ const insertar_usuario = async (req,resp)=>{
 
     try {
      
-        const usuario_existe = await usuarios.findOne({ email: datos.correo_usuario });
+        const usuario_existe = await usuario.findOne({ email: datos.correo_usuario });
 
         if (usuario_existe) {
             return resp.send({
@@ -63,7 +85,7 @@ const insertar_usuario = async (req,resp)=>{
             })
 
         }else{
-            const nuevo_usuario = new usuarios(datos)
+            const nuevo_usuario = new usuario(datos)
             await nuevo_usuario.save()
     
             return resp.send({
@@ -93,7 +115,7 @@ const editar_usuario = async (req,resp)=>{
     }
 
     try {
-        let consulta = await usuarios.findByIdAndUpdate(id,datos).exec()
+        await usuario.findByIdAndUpdate(id,datos).exec()
     
         return resp.send({
             completado: true,
@@ -113,7 +135,7 @@ const eliminar_usuario = async (req,resp)=>{
 
     try {
         
-        await usuarios.findByIdAndDelete(id).exec()
+        await usuario.findByIdAndDelete(id).exec()
 
         return resp.send({
             completado: true,
@@ -134,18 +156,18 @@ const recuperar_password = async (req,resp)=>{
     let correo_usuario = req.params.correo_usuario
 
     let datos = {
-        password_usuario: req.body.password_usuario
+        password_usuario: bcrypt.hashSync(req.body.password_usuario,10)
     }
 
     try {
+        let id = 0
+        usuario.find({correo_usuario:correo_usuario}).exec()
 
-        let id
-        let consulta = usuarios.find({correo_usuario:correo_usuario}).exec()
         .then(async(data)=>{
             id = data[0]._id
 
-            if (id) {
-                await usuarios.findByIdAndUpdate(id,datos).exec()
+            if (id != 0 || id != undefined || id != null) {
+                await usuario.findByIdAndUpdate(id,datos).exec()
                 return resp.send({
                     completado: true,
                     mensaje: `Contraseña actualizada`
@@ -169,11 +191,97 @@ const recuperar_password = async (req,resp)=>{
     }
 }
 
+const log_in = async (req,resp)=>{
+
+    let data = {
+        correo_usuario: req.body.correo_usuario,
+        password_usuario: req.body.password_usuario
+    }
+
+    let usuario_existe = await usuario.findOne({ correo_usuario: data.correo_usuario});
+    
+    try {
+
+        if (!usuario_existe) {
+            return resp.send({
+                completado: false,
+                mensaje: "No existe el usuario",
+            });
+            
+        } else {
+
+            let password_hash = usuario_existe.password_usuario
+
+            if (bcrypt.compareSync(data.password_usuario, password_hash)) {
+
+                const token = jwt.sign({
+                    userID :usuario_existe._id,
+                    isAdmin: usuario_existe.is_admin
+                },
+                "seCreTo",
+                {expiresIn:"30m"}
+                )
+
+                return resp.send({
+                    completado: true,
+                    mensaje: "ok",
+                    token: token
+                });
+
+            }else{
+                return resp.send({
+                    completado: false,
+                    mensaje: "Contraseña incorrecta",
+                });
+
+            }
+
+        }
+        
+
+    } catch (error) {
+        return resp.send({
+            completado: false,
+            mensaje:"Ocurrió un error: "+error
+        })
+    }
+
+}
+
+const verificar_correo = async(req,resp)=>{
+    let correo_usuario = req.params.correo_usuario
+
+    try {
+        const correo_existe = await usuario.exists({correo_usuario:correo_usuario}).exec()
+
+        if (correo_existe) {
+            return resp.send({
+                completado: true,
+                mensaje: `El correo existe en la base de datos`
+            })
+        }else{
+            return resp.status(400).send({
+                completado: false,
+                mensaje: `No se pudo encontrar este correo`
+            })
+        }
+        
+    } catch (error) {
+        return resp.status(400).send({
+            completado: false,
+            mensaje: `Error: ${error}`
+        })
+    }
+
+}
+
 module.exports = {
     listar_todos,
     listar_por_id,
     insertar_usuario,
     editar_usuario,
     eliminar_usuario,
-    recuperar_password
+    recuperar_password,
+    log_in,
+    verificar_correo
 }
--- back/routes/usuario.js
+++ back/routes/usuario.js
@@ -1,3 +1,11 @@
+/**
+ * @function rutas_usuarios
+ * @constant express - Instanciamos la libreria express para usar el metodo Router
+ * @constant router - Variable que instancia el metodo router de express, para poder utilizar los metodos de envio de request
+ * @constant usuarios - Importamos la ruta de los controllers, para acceder a todas las funciones
+ * @method router - Metodo que acepta una ruta como string, y una funcion que maneja todo lo que hace esa ruta
+ */
+
 const express = require('express')
 const router = express.Router()
 
@@ -9,5 +17,7 @@ router.post("/usuarios/insertar_usuario",usuarios.insertar_usuario)
 router.put("/usuarios/editar_usuario/:id",usuarios.editar_usuario)
 router.delete("/usuarios/eliminar_usuario/:id",usuarios.eliminar_usuario)
 router.post("/usuarios/rec_pass/:correo_usuario",usuarios.recuperar_password)
+router.post("/usuarios/log_in",usuarios.log_in)
+router.get("/usuarios/verificar_correo/:correo_usuario",usuarios.verificar_correo)
 
 module.exports  = router
